08523fd9060fda90373a7b4a8cc68a47c31bf6ec
NathanWaddell1211 <waddellnathan1211@gmail.com>
2019-05-14 20:57:10 -0500

    Added (3) validate middleware.

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ const server = express();
 
 server.use(express.json());
 
+// custom Middleware
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-
+  console.log(`${new Date().toISOString()}`)
+  console.log(`${req.method}`)
+  console.log(`${req.url}`)
+  next();
 };
 
 server.listen(port, () => {
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = 'express';
+const userDb = require('../users/userDb');
 
 const router = express.Router();
 
@@ -32,15 +33,58 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+    try{
+        const userId = await userDb.getById(req.params.id);
+        if(userId) {
+            req.userId = userId;
+            next();
+        } else {
+            res.status(404).json({
+                message: 'invalid user id'
+            })
+        }
+    }
+    catch(err) {
+        res.status(500).json({
+            message: 'Failed to process request'
+        })
+        next();
+    }    
 };
 
 function validateUser(req, res, next) {
-
+    if(req.body && Object.keys(req.body).length) {
+        if(req.body.name !== '') {
+            next();
+            res.status(400).json({
+                message: 'Missing required name field'
+            })
+            next();
+        }
+    } else {
+        res.status(400).json({
+            message: 'Missing user data'
+        })
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
+    if(req.body && Object.keys(req.body).length) {
+        if(req.body.text !== '') {
+            next();
+            res.status(400).json({
+                message: 'Missing required text field'
+            })
+            next();
+        }
+    } else {
+        res.status(400).json({
+            message: 'Missing post data'
+        })
+        next();
+    }
 
 };
 
